12ba43efb76eb6c6d228a1c54c04974286cd6334
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2026-09-23 16:36:55 -0700

    add pizzaPops

--- src/components/neighbourhood/index.tsx
+++ src/components/neighbourhood/index.tsx
@@ -1,13 +1,15 @@
 import { type JSX, useEffect, useState } from "react";
 
 type FoodKind =
-    | "beans"
+    // | "beans"
     | "burrito"
-    | "crunchies"
-    | "protein"
-    | "rice"
-    | "seeds"
-    | "side";
+    | "pizzaPops"
+    | "protein";
+// | "crunchies"
+// | "rice"
+// | "seeds"
+// | "side";
+
 type HouseDonation =
     & {
         [Kind in FoodKind as `${Kind}Amount`]: number;
@@ -31,13 +33,14 @@ function hasDonation() {
 
 function receiveDonation(foodKind: FoodKind): number {
     const FOODKIND_PRIMES_TABLE: Record<FoodKind, number[]> = {
-        "beans": [11, 13, 17, 19],
+        // "beans": [11, 13, 17, 19],
         "burrito": [1],
-        "crunchies": [17, 19, 23, 29, 31, 37, 41, 43, 47],
+        "pizzaPops": [1],
         "protein": [17, 19, 23, 29],
-        "rice": [41, 43, 47, 53, 59, 61, 67, 71, 73, 79],
-        "seeds": [17, 19, 23, 29],
-        "side": [23, 29, 31, 37, 41, 43, 47],
+        // "crunchies": [17, 19, 23, 29, 31, 37, 41, 43, 47],
+        // "rice": [41, 43, 47, 53, 59, 61, 67, 71, 73, 79],
+        // "seeds": [17, 19, 23, 29],
+        // "side": [23, 29, 31, 37, 41, 43, 47],
     };
 
     const PRIMES = FOODKIND_PRIMES_TABLE[foodKind];
@@ -55,13 +58,9 @@ function setNeighbourhoodDonationsCB(
     return Array.from({ length })
         .reduce<Map<HouseNumber, HouseDonation>>((acc, _curr, index) => {
             const state: HouseDonation = {
-                beansAmount: receiveDonation("beans"),
                 burritoAmount: receiveDonation("burrito"),
-                crunchiesAmount: receiveDonation("crunchies"),
+                pizzaPopsAmount: receiveDonation("pizzaPops"),
                 proteinAmount: receiveDonation("protein"),
-                riceAmount: receiveDonation("rice"),
-                seedsAmount: receiveDonation("seeds"),
-                sideAmount: receiveDonation("side"),
                 visited: false,
             };
             acc.set(index, state);
@@ -102,13 +101,9 @@ function Neighbourhood(): JSX.Element {
         housesRevealed: number,
     ): TotalAlms {
         const initialAcc: TotalAlms = {
-            beansTotal: 0,
             burritoTotal: 0,
-            crunchiesTotal: 0,
+            pizzaPopsTotal: 0,
             proteinTotal: 0,
-            riceTotal: 0,
-            seedsTotal: 0,
-            sideTotal: 0,
         };
 
         return Array.from(neighbourhoodDonations)
@@ -119,22 +114,14 @@ function Neighbourhood(): JSX.Element {
                     }
 
                     const {
-                        beansAmount,
                         burritoAmount,
-                        crunchiesAmount,
+                        pizzaPopsAmount,
                         proteinAmount,
-                        riceAmount,
-                        seedsAmount,
-                        sideAmount,
                     } = houseDonation;
 
-                    acc.beansTotal += beansAmount;
                     acc.burritoTotal += burritoAmount;
-                    acc.crunchiesTotal += crunchiesAmount;
+                    acc.pizzaPopsTotal += pizzaPopsAmount;
                     acc.proteinTotal += proteinAmount;
-                    acc.riceTotal += riceAmount;
-                    acc.seedsTotal += seedsAmount;
-                    acc.sideTotal += sideAmount;
 
                     return acc;
                 },
@@ -145,13 +132,9 @@ function Neighbourhood(): JSX.Element {
     const houses = Array.from(neighbourhoodDonations.values()).map(
         (houseDonation, index) => {
             const {
-                beansAmount,
                 burritoAmount,
-                crunchiesAmount,
+                pizzaPopsAmount,
                 proteinAmount,
-                riceAmount,
-                seedsAmount,
-                sideAmount,
                 visited,
             } = houseDonation;
 
@@ -159,26 +142,15 @@ function Neighbourhood(): JSX.Element {
                 ? (
                     <div key={String(index)} className="house visited">
                         <h3>🤗💐🌷🌹 House {index + 1} 🙏🌸🌺</h3>
-                        <p>
-                            {`Please have some beans: ${beansAmount}`}
-                        </p>
+
                         <p>
                             {`Please have some burritoes: ${burritoAmount}`}
                         </p>
                         <p>
-                            {`Please have some crunchies: ${crunchiesAmount}`}
+                            {`Please have some pizza pops: ${pizzaPopsAmount}`}
                         </p>
                         <p>
                             {`Please have some protein: ${proteinAmount}`}
-                        </p>
-                        <p>
-                            {`Please have some rice: ${riceAmount}`}
-                        </p>
-                        <p>
-                            {`Please have some seeds: ${seedsAmount}`}
-                        </p>
-                        <p>
-                            {`Please have some side: ${sideAmount}`}
                         </p>
                     </div>
                 )
@@ -196,25 +168,17 @@ function Neighbourhood(): JSX.Element {
     );
 
     const {
-        beansTotal,
         burritoTotal,
-        crunchiesTotal,
+        pizzaPopsTotal,
         proteinTotal,
-        riceTotal,
-        seedsTotal,
-        sideTotal,
     } = sumDonations(neighbourhoodDonations, housesRevealed);
 
     const neighbourhoodAlmsElement = (
         <div className="totals">
             <h2>Neighbourhood Alms</h2>
-            <p>{`Beans: ${beansTotal}`}</p>
             <p>{`Burritoes: ${burritoTotal}`}</p>
-            <p>{`Crunchies: ${crunchiesTotal}`}</p>
+            <p>{`Pizza Pops: ${pizzaPopsTotal}`}</p>
             <p>{`Protein: ${proteinTotal}`}</p>
-            <p>{`Rice: ${riceTotal}`}</p>
-            <p>{`Seeds: ${seedsTotal}`}</p>
-            <p>{`Side: ${sideTotal}`}</p>
         </div>
     );
 
